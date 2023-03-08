178a6e2f4ace5837c98ea5e2ebe39935b54dc2de
Febriqgal Purnama <febriqgalp@gmail.com>
2023-03-08 23:58:54 +0700

    mmmmmmm

--- src/pages/login/signup.js
+++ src/pages/login/signup.js
@@ -108,7 +108,7 @@ export default function Signup() {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-800"
               >
-                Daftar
+                DaftaR
               </button>
             </div>
           </form>
